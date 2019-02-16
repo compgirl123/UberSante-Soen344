e1e8d40e6e73d2cf26abe19fe060e41eb2ccc04c
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2019-02-15 22:51:18 -0500

    Revert "Merge pull request #2 from compgirl123/Database-Setup"
    This reverts commit 8a67e9cb7e807e65d239f1704f6935a7bd97c743, reversing
    changes made to 4f9d435d59c1cd4489afe7134ff40a7f3c6f039e.

--- _updated/run.py
+++ _updated/run.py
@@ -1,11 +1,7 @@
-import pytest
+from app import create_app
 
-from app import app
 
-# When put under app.run, wouldn't run until server would be shut off
-#pytest.main()
+app = create_app('config.development')
 
-# Runs the app with a direct call "python __init__.py"
-if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False) #had to put the use_reloader=False, because when app runs, it reloads and fills the database twice?
-
+if __name__ == '__main__':
+    app.run()
