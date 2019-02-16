05233294b6871a1ed70780f0493294998c989fae
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2019-02-15 22:51:29 -0500

    Revert "Revert "Set up Data Base #1""
    This reverts commit f1ef497d0888a55d25cad5793d1972ff6473b9ec.

--- _updated/run.py
+++ _updated/run.py
@@ -1,7 +1,11 @@
-from app import create_app
+import pytest
 
+from app import app
 
-app = create_app('config.development')
+# When put under app.run, wouldn't run until server would be shut off
+#pytest.main()
 
-if __name__ == '__main__':
-    app.run()
+# Runs the app with a direct call "python __init__.py"
+if __name__ == "__main__":
+    app.run(debug=True,use_reloader=False) #had to put the use_reloader=False, because when app runs, it reloads and fills the database twice?
+
