a1c47b12201ee9879b155b70367918bf39e35d1e
kasthurie <sriahila@hotmail.com>
2019-02-27 19:06:59 -0500

    #15 : Added database synching functionality

--- app/controllers/nursecontroller.py
+++ app/controllers/nursecontroller.py
@@ -1,4 +1,5 @@
 import sqlite3
+import sqlite3 as mysql
 import sys
 from sqlite3 import Error
 import app.classes.database_container
@@ -39,6 +40,32 @@ class Nursecontroller:
         print(values_from_db)
         return values_from_db
 
-    def find_user(self, name, password):
-        print("HELLOO")
-        return 0
+    def find_user(self, access_id, password):
+        # Make an sql query to search for the name and pasword instead of seleting all 
+        #print("HELLOO")
+        #return 0
+        
+        self.access_id = input
+        self.password = input
+
+        a = data()
+        with mysql.connect("app/database/SOEN344_DATABASE.db") as con:
+
+            loadaccess_id = ("select access_id from nurse where access_id = '%s'")
+            mycursor = con.cursor()
+            mycursor.execute(a.access_id, loadaccess_id)
+            access_idcheck = mycursor.fetchone()
+        
+            loadpassword = ("select password from nurse where password = '%s'")
+            mycursor2 = con.cursor()
+            mycursor2.execute(a.password,loadpassword)
+            passwordcheck = mycursor2.fetchone()
+
+        if a.access_id == access_idcheck and a.password == passwordcheck:
+            print ("welcome")
+
+        else:
+            print ("sorry please try again")
+        con.commit()
+        con.close()
+        
